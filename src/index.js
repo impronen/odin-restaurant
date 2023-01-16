4a42a3440dd2903888021262195ed68e11079a7b
impronen <impronen@gmail.com>
2023-01-16 14:48:57 +0200

    file & folder creation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import './style.css';
+import './styles/style.css';
+
+function testFunction() {
+    const element = document.createElement('div');
+    element.innerHTML = "Hey so does this work"
+    element.classList.add('test')
+    return element;
+
+}
+
+
+document.body.appendChild(testFunction());
