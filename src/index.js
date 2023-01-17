eda8933c33be0d4491703044baeb1924122edbe5
impronen <impronen@gmail.com>
2023-01-17 14:14:16 +0200

    Font

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 import './styles/style.css';
 
-function testFunction() {
-    const element = document.createElement('div');
-    element.innerHTML = "Hey so does this work"
-    element.classList.add('test')
-    return element;
+function headerGenerator() {
+    const header = document.createElement('div')
+    header.classList.add('header')
 
+    const headingLogo = document.createElement('div')
+    headingLogo.classList.add('headingLogo')
+    headingLogo.setAttribute('id', 'buttonContainer')
+    headingLogo.innerHTML = `<h1>The Jolly Badger</h1>`
+    header.appendChild(headingLogo)
+
+    const headingMenu = document.createElement('div')
+    headingMenu.classList.add('headingMenu')
+    headingMenu.setAttribute('id', 'headingMenu')
+    header.appendChild(headingMenu)
+
+    console.log(headingMenu)
+
+    return header;
+}
+
+function buttonGenerator() {
+    
+    let homeBtn = document.createElement('button')
+    homeBtn.innerHTML = "Hello!"
+    homeBtn.classList.add('headingButton')
+    menuVar.appendChild('homeBtn')
+    
+    const menuBtn = document.createElement('button')
+    menuBtn.classList.add('headingButton')
+    menuVar.appendChild('menuBtn')
+
+    const contactBtn = document.createElement('button')
+    contactBtn.classList.add('headingButton')
+    menuVar.appendChild('contactBtn') 
 }
 
 
-document.body.appendChild(testFunction());
+let content = document.getElementById('content')
+content.appendChild(headerGenerator());
+
+let menuVar = document.getElementById('buttonContainer')
+let headingMenu = document.getElementById('headingMenu')
+
+headingMenu.appendChild(buttonGenerator())
